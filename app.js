5e27c8fb7aef63762cb6f667149a4f4a7ce3ec8e
Marc Farra <marcfarra@gmail.com>
2014-02-05 22:50:32 -0500

    rollback changes

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
  * Module dependencies.
  */
 
- process.env.PWD = process.cwd()
-
 var express = require('express'),
 	engine = require('ejs-locals'),
 		projects = require('./models/projects'),
@@ -18,20 +16,19 @@ var express = require('express'),
 var app = express();
 
 // all environments
-var port = Number(process.env.PORT || 3000)
-app.set('port', port);
-app.set('views', path.join(process.env.PWD, 'views'));
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
-app.use(compass({ cwd: process.env.PWD + 'public' }));
+app.use(compass({ cwd: __dirname + 'public' }));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(compass({ cwd: process.env.PWD + 'public' }));
-app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(compass({ cwd: __dirname + 'public' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
